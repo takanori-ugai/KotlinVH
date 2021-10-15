dcad4a9ea41f0b8a65a8123facedf77d253ca1aa
Takanori Ugai <ugai@fujitsu.com>
2021-10-15 23:05:18 +0900

    add actions

--- src/main/kotlin/com/fujitsu/labs/virtualhome/Main.kt
+++ src/main/kotlin/com/fujitsu/labs/virtualhome/Main.kt
@@ -19,47 +19,84 @@ fun main(args: Array<String>) {
 
     /**
      * Not supported action in Unity Simulater
-     *   EAT
+     **  EAT
+     **  CUT
+     **  PUTIN
+     **  WIPE
+     **  PUTON
+     **  PUTOFF
+     **  GREET
+     **  DROP
+     **  LIE
+     **  POUR
+     **  TYPE
+     *   PUSH
+     *   PULL
+     **  MOVE
+     **  WASH
+     *   RINSE
+     **  SLEEP
+     **  WAKEUP
+     *   SCRUB
+     **  SQUEEZE
+     *   RELEASE
+     **  PLUGIN
+     **  PLUGOUT
      * Supported action
-     *   FIND
-     *   WALK
-     *   DRINK
-     *   CLOSE // ドアをちゃんと閉めない
-     *   READ
-     *   SIT
-     *   STANDUP
-     *   PUTBACK
-     *   GRAB
-     *   LOOKAT
+     **  FIND
+     **  WALK
+     **  DRINK
+     **  CLOSE // ドアをちゃんと閉めない。ドアしか閉められない
+     **  READ
+     **  SIT
+     **  STANDUP
+     **  PUTBACK
+     **  GRAB
+     **  LOOKAT
      *   LOOKAT_SHORT
      *   LOOKAT_MEDIUM = LOOKAT
      *   LOOKAT_LONG
-     *
+     **  OPEN // ドアしか開けられない
+     **  POINTAT = LOOKAT
+     *   PUTOBJBACK // 手に持っているものを元あった場所に戻す
+     *   RUN
+     **  SWITCHOFF
+     **  SWITCHON
+     **  TOUCH
+     **  TURNTO
+     **  WATCH
      */
     val script0 = listOf(
 //        "<char0> [WALK] <wine> (93)",
-//        "<char0> [FIND] <wine> (93)",
+//        "<char0> [GRAB] <wine> (93)",
 //        "<char0> [DRINK] <wine> (93)",
-        "<char0> [WALK] <kitchencabinet> (146)",
-        "<char0> [OPEN] <kitchencabinet> (146)",
+//        "<char0> [WALK] <washingmachine> (365)",
+//        "<char0> [PUTIN] <washingmachine> (365)  <wine> (93)",
 //        "<char0> [CLOSE] <door> (128)",
 //        "<char0> [LOOKAT_LONG] <door> (128)",
 //        "<char0> [OPEN] <door> (128)",
-        "<char0> [WALK] <book> (86)",
+//        "<char0> [WALK] <sofa> (25)",
 //        "<char0> [FIND] <book> (86)",
-//        "<char0> [READ] <book> (86)",
-        "<char0> [WALK] <sofa> (139)",
+//        "<char0> [PULL] <sofa> (25)",
+//        "<char0> [GRAB] <book> (86)",
+//        "<char0> [WALK] <tv> (106)",
+//        "<char0> [TOUCH] <tv> (106)",
+//        "<char0> [TURNTO] <tv> (106)",
+//        "<char0> [WIPE] <tv> (106)",
 //        "<char0> [FIND] <sofa> (139)",
-        "<char0> [SIT] <sofa> (139)",
-        "<char0> [STANDUP] <sofa> (139)",
-        "<char0> [PUTBACK] <book> (86) <sofa> (139)",
-        "<char0> [GRAB] <book> (86)"
+//        "<char0> [SIT] <sofa> (139)",
+//        "<char0> [STANDUP] <sofa> (139)",
+        "<char0> [WALK] <tablelamp> (76)",
+//        "<char0> [PUTON] <clothesshirt> (36)"
+//        "<char0> [GRAB] <book> (86)",
+        "<char0> [PLUGIN] <tablelamp> (76)"
     )
     val main = Main()
 //    main.checkScripts(script0)
-//    main.testScripts(script0)
-    println(main.findNodes("kitchencabinet"))
-    println(main.findNodesByProperty("CAN_OPEN"))
+    main.testScripts(script0)
+    println(main.findNodes("tv"))
+    println(main.findNodesByProperty("HAS_PLUG"))
+    println(main.findNodesById(1))
     System.exit(0)
 
     val data = VirtualHomeRequest(currentTimeMillis().toInt(), "idle")
@@ -128,6 +165,7 @@ class Main {
         if(! client.addCharacter()!!.success) throw Exception("Add Character Error")
         val graph = client.environmentGraph()
 //        val catId = graph.nodes.filter { it.class_name == "cat"}[0]
+//        println("CATID: $catId")
         val config = RenderParams(
             processing_time_limit = 1, find_solution = false, skip_animation = false, recording = true,
             save_pose_data = false, skip_execution = false
@@ -137,7 +175,7 @@ class Main {
 //        save_pose_data = false, image_synthesis = listOf()
 //    )
 //        val scriptObj = Script(script)
-        if(! client.renderScript(script, config)!!.success) throw Exception("Error in Rendering")
+        println(client.renderScript(script, config))
         return true
     }
 
@@ -163,6 +201,7 @@ class Main {
 
     fun findNodes(name: String) : List<Node> {
         if(! client.reset(4)!!.success) throw Exception("Reset Error")
+        if(! client.addCharacter()!!.success) throw Exception("Add Character Error")
         val graph = client.environmentGraph()
         val regex = Regex(name)
         return graph.nodes.filter {
@@ -176,9 +215,19 @@ class Main {
 
     fun findNodesByProperty(property: String) : List<Node> {
         if(! client.reset(4)!!.success) throw Exception("Reset Error")
+        if(! client.addCharacter()!!.success) throw Exception("Add Character Error")
         val graph = client.environmentGraph()
         return graph.nodes.filter {
             it.properties != null && it.properties.contains(property)
+        }
+    }
+
+    fun findNodesById(id: Int) : List<Node> {
+        if(! client.reset(4)!!.success) throw Exception("Reset Error")
+        if(! client.addCharacter()!!.success) throw Exception("Add Character Error")
+        val graph = client.environmentGraph()
+        return graph.nodes.filter {
+            it.id != null && it.id == id
         }
     }
 
