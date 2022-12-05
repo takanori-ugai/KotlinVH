996ac4d340c56c13b589ae9bade3ff0cc8442849
Takanori Ugai <ugai@fujitsu.com>
2022-12-05 16:12:35 +0900

    gradle version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import io.gitlab.arturbosch.detekt.Detekt
 import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 plugins {
-    kotlin("jvm") version "1.7.20"
-    kotlin("plugin.serialization") version "1.7.20"
+    kotlin("jvm") version "1.7.21"
+    kotlin("plugin.serialization") version "1.7.21"
     java
     id("com.github.johnrengelman.shadow") version "7.1.2"
     jacoco
