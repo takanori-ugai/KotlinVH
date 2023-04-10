7e3eef308744650630bc0705092df03fe15ad8c6
Takanori Ugai <ugai@fujitsu.com>
2023-04-10 18:51:15 +0900

    kotlin version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,19 +3,19 @@ import io.gitlab.arturbosch.detekt.Detekt
 import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 plugins {
-    kotlin("jvm") version "1.8.0"
-    kotlin("plugin.serialization") version "1.8.0"
+    kotlin("jvm") version "1.8.20"
+    kotlin("plugin.serialization") version "1.8.20"
     java
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     jacoco
-    id("org.jetbrains.dokka") version "1.7.20"
+    id("org.jetbrains.dokka") version "1.8.10"
     id("io.gitlab.arturbosch.detekt") version "1.22.0"
 //    id("com.github.sherter.google-java-format") version "0.9"
 //    kotlin("jupyter.api") version "0.10.1-8"
-    id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
     id("com.github.jk1.dependency-license-report") version "2.1"
-    id("com.github.spotbugs") version "5.0.13"
-    id("com.diffplug.spotless") version "6.12.0"
+    id("com.github.spotbugs") version "5.0.14"
+    id("com.diffplug.spotless") version "6.18.0"
 }
 
 group = "com.fujitsu"
@@ -114,7 +114,7 @@ spotbugs {
 }
 
 jacoco {
-    toolVersion = "0.8.8"
+    toolVersion = "0.8.9"
 //    reportsDirectory.set(layout.buildDirectory.dir("customJacocoReportDir"))
 }
 
@@ -127,7 +127,7 @@ spotless {
         removeUnusedImports()
 
         // Choose one of these formatters.
-        googleJavaFormat("1.15.0")
+        googleJavaFormat("1.16.0")
         formatAnnotations()
     }
 }
