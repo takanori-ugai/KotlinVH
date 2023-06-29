431796f9a5a9f3e52fdde73edd9051a3cb803cb8
Takanori Ugai <ugai@fujitsu.com>
2023-06-29 23:08:46 +0900

    add some tests

--- src/test/kotlin/com/fujitsu/labs/virtualhome/RequestTest.kt
+++ src/test/kotlin/com/fujitsu/labs/virtualhome/RequestTest.kt
@@ -6,7 +6,7 @@ import com.github.tomakehurst.wiremock.junit5.WireMockTest
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 @WireMockTest
@@ -46,5 +46,44 @@ class RequestTest {
         val port = info.httpPort
         val vh = VirtualHomeClient(port = port)
         assertTrue(vh.reset(sceneIndex)?.success!!)
+        assertTrue(vh.reset()?.success!!)
+    }
+
+    @Test
+    fun cameraCountTest(info: WireMockRuntimeInfo) {
+        val value = 1
+        val res = VirtualHomeResponse(1, true, "test", value, listOf("Test"))
+        stubFor(
+            post("/")
+                .withRequestBody(matchingJsonPath("$[?(@.action == 'camera_count')]"))
+                .willReturn(okJson(format.encodeToString(res)))
+        )
+        val port = info.httpPort
+        val vh = VirtualHomeClient(port = port)
+        assertEquals(value, vh.cameraCount())
+    }
+
+    @Test
+    fun errorTest(info: WireMockRuntimeInfo) {
+        val vh = VirtualHomeClient(port = info.httpPort + 100)
+        assertFalse(vh.check()!!.success)
+    }
+
+    @Test
+    fun visibleObjectTest(info: WireMockRuntimeInfo) {
+        val value = 1
+        val map: Map<String, String> = mapOf("0" to "1")
+        val res = VirtualHomeResponse(1, true, format.encodeToString(map), value, listOf("Test"))
+        stubFor(
+            post("/")
+                .withRequestBody(
+                    matchingJsonPath("$[?(@.action == 'observation')]")
+                        .and(matchingJsonPath("$[?(@.intParams[0] == $value)]"))
+                )
+                .willReturn(okJson(format.encodeToString(res)))
+        )
+        val port = info.httpPort
+        val vh = VirtualHomeClient(port = port)
+        assertEquals(map, vh.visibleObjects(value))
     }
 }
