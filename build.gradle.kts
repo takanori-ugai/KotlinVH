f3bf3d8f6fa2c720af9d4e48dac73781217b1147
Takanori Ugai <ugai@fujitsu.com>
2023-07-07 14:00:27 +0900

    plugin version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     id("io.gitlab.arturbosch.detekt") version "1.23.0"
 //    id("com.github.sherter.google-java-format") version "0.9"
 //    kotlin("jupyter.api") version "0.10.1-8"
-    id("org.jlleitschuh.gradle.ktlint") version "11.4.0"
+    id("org.jlleitschuh.gradle.ktlint") version "11.5.0"
     id("com.github.jk1.dependency-license-report") version "2.1"
     id("com.github.spotbugs") version "5.0.14"
     id("com.diffplug.spotless") version "6.19.0"
