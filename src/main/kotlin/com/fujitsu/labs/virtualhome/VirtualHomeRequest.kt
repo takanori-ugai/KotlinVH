2b79dfa6b06e25d5770ac1032a3018f683564ab2
Takanori Ugai <ugai@fujitsu.com>
2023-07-21 10:10:43 +0900

    documents update

--- src/main/kotlin/com/fujitsu/labs/virtualhome/VirtualHomeRequest.kt
+++ src/main/kotlin/com/fujitsu/labs/virtualhome/VirtualHomeRequest.kt
@@ -4,6 +4,14 @@ import kotlinx.serialization.*
 import kotlinx.serialization.builtins.*
 import kotlinx.serialization.json.*
 
+/**
+ * Represents a request to the VirtualHome server.
+ *
+ * @property id The ID of the request, which defaults to the current system time in milliseconds.
+ * @property action The action to perform.
+ * @property intParams Optional list of integer parameters.
+ * @property stringParams Optional list of string parameters.
+ */
 @Serializable
 data class VirtualHomeRequest(
     val id: Int = System.currentTimeMillis().toInt(),
--- src/main/kotlin/com/fujitsu/labs/virtualhome/VirtualHomeResponse.kt
+++ src/main/kotlin/com/fujitsu/labs/virtualhome/VirtualHomeResponse.kt
@@ -3,6 +3,15 @@ package com.fujitsu.labs.virtualhome
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+/**
+ * Represents a response from the VirtualHome server.
+ *
+ * @property id The ID of the response.
+ * @property success Indicates whether the request was successful.
+ * @property message The message accompanying the response, if any.
+ * @property value The value associated with the response.
+ * @property messageList A list of messages associated with the response, if any.
+ */
 @Serializable
 data class VirtualHomeResponse(
     val id: Int,
