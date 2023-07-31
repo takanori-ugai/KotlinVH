5c80502946cf4dd93a31a13f8e6cf2a781308ef6
Takanori Ugai <ugai@fujitsu.com>
2023-07-31 15:32:52 +0900

    docs: Add Dokka comments to main function, writeNodesToCSV, and GetObjects class

--- src/main/kotlin/com/fujitsu/labs/virtualhome/GetObjects.kt
+++ src/main/kotlin/com/fujitsu/labs/virtualhome/GetObjects.kt
@@ -2,18 +2,36 @@ package com.fujitsu.labs.virtualhome
 
 import java.io.File
 
+/**
+ * The main function that initializes the client, resets the scene, retrieves the environment graph,
+ * and writes the nodes to a CSV file.
+ */
 fun main() {
-    val sceneNum = 3
-    val client = VirtualHomeClient(host = "localhost")
     val sceneIndex = 6
+    val client = VirtualHomeClient(host = "localhost")
     client.reset(sceneIndex)
     val graph = client.environmentGraph()
-    File("scene${sceneIndex + 1}.csv").bufferedWriter(Charsets.UTF_8).use { out ->
-        out.write("nodes/id,nodes/class_name\n")
-        println("nodes/id,nodes/class_name")
-        graph.nodes.forEach {
-            out.write("${it.className},${it.id}\n")
+    writeNodesToCSV(graph.nodes, "scene${sceneIndex + 1}.csv")
+}
+
+/**
+ * Writes the list of nodes to a CSV file.
+ *
+ * @param nodes The list of nodes to be written to the CSV file.
+ * @param fileName The name of the CSV file.
+ */
+private fun writeNodesToCSV(nodes: List<Node>, fileName: String) {
+    File(fileName).bufferedWriter(Charsets.UTF_8).use { out ->
+        val header = "nodes/id,nodes/class_name"
+        out.write("$header\n")
+        println(header)
+        nodes.forEach { node ->
+            out.write("${node.className},${node.id}\n")
         }
     }
 }
+
+/**
+ * A class named GetObjects. Currently, it doesn't contain any properties or methods.
+ */
 class GetObjects
