2738c5ac9cb03ac89c778e0de4f54345971a4af4
Takanori Ugai <ugai@fujitsu.com>
2025-01-21 11:15:06 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -129,7 +129,7 @@ spotless {
         removeUnusedImports()
 
         // Choose one of these formatters.
-        googleJavaFormat("1.24.0")
+        googleJavaFormat("1.25.2")
         formatAnnotations()
     }
 }
