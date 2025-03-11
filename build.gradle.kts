df70fe42710032d300ba8bdf9d4345f5fbc83f86
Takanori Ugai <ugai@fujitsu.com>
2025-03-11 22:21:16 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,9 +29,9 @@ dependencies {
 //    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.+")
     implementation("ch.qos.logback:logback-classic:1.5.17")
     testImplementation("org.wiremock:wiremock:3.12.1")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.4")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.4")
-}
+    testImplementation(platform("org.junit:junit-bom:5.12.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")}
 
 tasks {
     compileKotlin {
