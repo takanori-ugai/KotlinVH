acb9e7eab9724792e91d553343cb48bb24f398be
Takanori Ugai <ugai@fujitsu.com>
2026-03-18 19:27:07 +0900

    ⬆️ chore(ci): update gradle workflow for Java 25 support and adjust setup-gradle version ➕ build(gradle): add org.jetbrains.dokka-javadoc plugin version 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     id("com.gradleup.shadow") version "9.3.2"
     jacoco
     id("org.jetbrains.dokka") version "2.1.0"
+    id("org.jetbrains.dokka-javadoc") version "2.0.0"
     id("io.gitlab.arturbosch.detekt") version "1.23.8"
     id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
     id("com.github.jk1.dependency-license-report") version "3.1.1"
