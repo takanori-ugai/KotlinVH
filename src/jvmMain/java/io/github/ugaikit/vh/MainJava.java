9ef14f24926ad6fd966533e9c8b0619848bbda34
Takanori Ugai <ugai.takanori@gmail.com>
2026-06-13 14:23:24 +0900

    reformat

--- src/jvmMain/java/io/github/ugaikit/vh/MainJava.java
+++ src/jvmMain/java/io/github/ugaikit/vh/MainJava.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 public class MainJava {
   /** Sample constants used to keep this demo readable. */
   private static final int RESET_NUM = 4;
+
   private static final int POS_X = 10;
   private static final int POS_Y = 200;
   private static final int POS_Z = 5;
